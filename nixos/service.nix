f7b93deda9a7cc5a9a0216e865fdfd42dd214bb6
blcklarper <244165516+blcklarper@users.noreply.github.com>
2026-07-29 15:02:46 +0200

    deleted bugged files

--- nixos/service.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }:
-{
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  services.printing.enable = true;
-  services.openssh.enable = true;
-}
